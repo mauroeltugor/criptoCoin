2272ecb1c5f45650deaaff8ddc3b436369307e03
mauro <andrekxus299@gmail.com>
2023-05-31 17:00:12 -0500

    create target

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -4,28 +4,54 @@ import search from "../getData";
 const Try =  () =>{
 
     const[data, setData] = useState([]);
+
+    
     
     const fetchData = async () => {
         const newData = await search();
         setData(newData)
     }
 
-    console.log(data);
+    fetchData()
 
-    if(data.length ===0){
-        fetchData();
-    }
+    const FilterCoin = data.filter((event) => event.name.toLowerCase().includes(search.toLowerCase()));
 
     const dataInfo = data.map((element) => {
-        return <p>{element.symbol}</p>; // Suponiendo que el objeto tiene una propiedad "nombre"
+        return(
+            <div>
+                <div >
+                    <div className="criptoInfo">
+                        <p className="symbol">{element.symbol}</p>
+                        <p>{element.name}</p>
+                        <p>${element.current_price}</p>
+                    </div>
+               </div>
+            </div>
+        )  
       });
 
     return(
-        <div>
-            <ul>
-                <li>{dataInfo}</li>
-            </ul>
-        </div>
+        <section className="main">
+            <div className="target">
+                <div className="startPart">
+                    <h1>HELLO WORLD</h1>
+                </div>
+                <div className="endPart">
+                    <div className="tittleSearch">
+                        <h1>Control Panel</h1>
+                        <input className="searchBar" type="text" onChange={FilterCoin} placeholder="Enter your search request..." />
+                    </div>
+                    <div className="b2b">
+                        <h3>B2B DISTRIBUTION</h3>
+                        <p>Sales Deal</p>
+                    </div>
+                    <hr className="line"/>
+                    <div>
+                        {dataInfo}
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
--- src/getData.js
+++ src/getData.js
@@ -1,16 +1,17 @@
 const search = async () => {
-    const url = 'https://api.coingecko.com/api/v3/search?query=a';
+    const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1';
     const response = await fetch(url);
     const data = await response.json();
-    const finalData = data.coins
-    const final = finalData.map((data) => {
+    const final = data.map((data) => {
       return{
-        'name': data.image,
+        'name': data.name,
         'symbol': data.symbol,
-        'image':data.image
+        'image':data.image,
+        'current_price': data.current_price
       }
     })
+    console.log(final);
     return final;
   };
   
-  export default search;
+export default search;
