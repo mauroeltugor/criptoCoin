b2a0abe1eb606ee78988510a8387f7337f9ffb55
mauro <andrekxus299@gmail.com>
2023-06-09 15:50:20 -0500

    change symbol style change search bar using other endpoint and add the useeffect and create a new readme

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import SearchCoinsBar from './components/searchBar';
 
--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -1,89 +1,75 @@
-import { useEffect, useState } from "react";
-import searchInfo from "../helper/getCriptoCoinData";
-import search from "../helper/search";
+import React, { useState, useEffect } from "react";
 
-const SearchCoinsBar =  () =>{
-    
-    const[data, setData] = useState([]);
-    const [searchTerm, setSearchTerm] = useState("");
-    const[searchData, setSearchData] = useState([]);
-    
-    // const FilterCoin = (event) => {
-    //     setSearchTerm(event.target.value);
-    // };
-    
-    // const filteredData = data.filter((element) =>
-    // element.symbol.toLowerCase().includes(searchTerm.toLowerCase()) ||
-    // element.name.toLowerCase().includes(searchTerm.toLowerCase())
-    // );
-    
-    // const end= `https://api.coingecko.com/api/v3/search?query=${filteredData}`
+const SearchCoinsBar = () => {
+  const [data, setData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://api.coingecko.com/api/v3/search?query=${searchTerm}`
+        );
+        const newData = await response.json();
+        setData(newData.coins);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-    // const searchResult = async () => {
-    //     const searchData = await search(end); 
-    //     setSearchData(searchData);      
-    // };
-       
+    fetchData();
+  }, [searchTerm]);
 
-    useEffect(()=>{
-        const fetchData = async () => {
-            const newData = await searchInfo();
-            setData(newData)
-        }
-        fetchData()
-    })
+  const FilterCoin = (event) => {
+    setSearchTerm(event.target.value);
+  };
 
-    // useEffect(()=>{
-    //     const searchFetch = async(url) =>{
-    //         const dataResultApi = await searchResult(url);
-    //         setSearchData(dataResultApi);
-    //     }
-    //     searchFetch(end)
-    // })
-      
-    // useEffect(() => {
-    //     searchResult(); 
-    //   }, [filteredData]);
-      
+  const filteredData = data.filter(
+    (element) =>
+      element.symbol.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      element.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
-    const dataInfo = data.map((element) => {
-        return(
-            <div>
-                <div >
-                    <div className="criptoInfo">
-                        <img src={element.image} className="symbol"/>
-                        <p>{element.name}</p>
-                        <p>${element.current_price}</p>
-                    </div>
-               </div>
-            </div>
-        )  
-      });
+  const dataInfo = filteredData.map((element) => {
+    return (
+      <div key={element.id}>
+        <div>
+          <div className="criptoInfo">
+            <img src={element.image} className="symbol" alt={element.name} />
+            <p>{element.name}</p>
+            <p>${element.current_price}</p>
+          </div>
+        </div>
+      </div>
+    );
+  });
 
-    return(
-        <section className="main">
-            <div className="target">
-                <div className="startPart">
-                    <h1>Sales Activity</h1>
-                </div>
-                <div className="endPart">
-                    <div className="tittleSearch">
-                        <h1>Control Panel</h1>
-                        <input className="searchBar" type="text" /*onChange={FilterCoin}*/ placeholder="Enter your search request..." />
-                    </div>
-                    <div className="b2b">
-                        <h3>B2B DISTRIBUTION</h3>
-                        <p>Sales Deal</p>
-                    </div>
-                    <hr className="line"/>
-                    <div>
-                        {dataInfo}
-                    </div>
-                </div>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section className="main">
+      <div className="target">
+        <div className="startPart">
+          <h1>Sales Activity</h1>
+        </div>
+        <div className="endPart">
+          <div className="tittleSearch">
+            <h1>Control Panel</h1>
+            <input
+              className="searchBar"
+              type="text"
+              onChange={FilterCoin}
+              placeholder="Enter your search request..."
+            />
+          </div>
+          <div className="b2b">
+            <h3>B2B DISTRIBUTION</h3>
+            <p>Sales Deal</p>
+          </div>
+          <hr className="line" />
+          <div>{dataInfo}</div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default SearchCoinsBar;
